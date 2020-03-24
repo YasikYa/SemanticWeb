2e7de957df05a7cc6cf402ac7515b208385a81ee
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2020-03-24 22:09:02 +0200

    init DocumentEditor

---
+++ Lab1/XMLParsing/XMLParsing/XML/IDocumentEditor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMLParsing.XML
+{
+    interface IDocumentEditor
+    {
+        void AddAttribute();
+        void AddTextValue();
+        void AddComplexTypeElement();
+    }
+}
---
+++ Lab1/XMLParsing/XMLParsing/XML/XmlEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace XMLParsing.XML
+{
+    class XmlEditor : IDocumentEditor
+    {
+        private readonly string _xmlPath;
+        public XmlEditor(string documentPath) => _xmlPath = documentPath;
+        
+        public void AddAttribute()
+        {
+            var doc = LoadDocument();
+            var vacancyElements = doc.GetElementsByTagName("Vacancy");
+            var nextId = 1;
+            foreach(XmlElement vacancy in vacancyElements)
+            {
+                vacancy.SetAttribute("customId", nextId.ToString());
+                nextId++;
+            }
+        }
+
+        public void AddComplexTypeElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTextValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        private XmlDocument LoadDocument()
+        {
+            var document = new XmlDocument();
+            document.Load(_xmlPath);
+            return document;
+        }
+    }
+}
